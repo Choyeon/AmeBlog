2115e3f6d52f8bb12d890472f1140b54272c8a26
雨过初晴 <admin@choyeon.cn>
2020-07-28 21:07:00 +0800

    ⚙️完成扩展包的基本功能

--- app/extensions.py
+++ app/extensions.py
@@ -3,3 +3,14 @@
 # @Email   : admin@choyeon.cn
 # @Site    : https://www.choyeon.cn
 # @File    : extensions
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail
+from flask_ckeditor import CKEditor
+from flask_moment import Moment
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+moment = Moment()
+ckeditor = CKEditor()
+mail = Mail()
