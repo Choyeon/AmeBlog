7ad38bede581298307cc22d1ac90bb5197ae863e
雨过初晴 <admin@choyeon.cn>
2020-07-29 10:39:55 +0800

    📦完成文章相关数据模型

--- app/models.py
+++ app/models.py
@@ -3,6 +3,8 @@
 # @Email   : admin@choyeon.cn
 # @Site    : https://www.choyeon.cn
 # @File    : models
+from datetime import datetime
+
 from .extensions import db
 
 
@@ -14,3 +16,32 @@ class Admin(db.Model):
     blog_sub_title = db.Column(db.String(100))
     name = db.Column(db.String(30))
     about = db.Column(db.Text)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    body = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    category_id = db.Column(db.Integer, db.ForeignKey('category_id'))
+    category = db.relationship('Category', back_populates='posts')
+    comments = db.relationship('Comment', backref='post', cascade='all,de;ete-orphan')
+
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), unique=True)
+    post = db.relationship('Post', back_populates='category')
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String(30))
+    email = db.Column(db.String(254))
+    site = db.Column(db.String(255))
+    body = db.Column(db.Text)
+    from_admin = db.Column(db.Boolean, default=False)
+    reviewed = db.Column(db.Boolean, default=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    post_id = db.Column(db.Integer, db.ForeignKey('Post'))
+    post = db.relationship('Post', back_populates='comments')
