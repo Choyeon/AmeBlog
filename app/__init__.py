3a5d6e11a523cf317ffb47bb8021f35dd5654539
雨过初晴 <admin@choyeon.cn>
2020-07-29 08:29:50 +0800

    🏭完成组织工厂函数

--- app/__init__.py
+++ app/__init__.py
@@ -5,9 +5,12 @@
 # @File    : __init__.py
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 
+from app.blueprints.admin import admin_bp
+from app.blueprints.auth import auth_bp
 from app.blueprints.blog import blog_bp
+from app.extensions import bootstrap, db, ckeditor, mail, moment
 from app.settings import config
 
 
@@ -17,8 +20,52 @@ def create_app(config_name=None):
 
     app = Flask('Ameblog')
     app.config.from_object(config[config_name])
+    register_logging(app)
+    register_extensions(app)
+    register_blueprints(app)
+    register_commands(app)
+    register_errors(app)
+    register_shell_context(app)
+    register_template_context(app)
+    return app
 
+
+def register_logging(app):
+    # 注册日志
+    pass
+
+
+def register_extensions(app):
+    # 注册flask扩展
+    bootstrap.init_app(app)
+    db.init_app(app)
+    ckeditor.init_app(app)
+    mail.init_app(app)
+    moment.init_app(app)
+
+
+def register_blueprints(app):
+    # 注册蓝图
     app.register_blueprint(blog_bp)
     app.register_blueprint(admin_bp, url_prefix='/admin')
     app.register_blueprint(auth_bp, url_prefix='/auth')
-    return app
+
+
+def register_commands(app):
+    pass
+
+
+def register_errors(app):
+    @app.errorhandler(400)
+    def bad_request(e):
+        return render_template('errors/400.html'), 400
+
+    ...
+
+
+def register_shell_context(app):
+    pass
+
+
+def register_template_context(app):
+    pass
