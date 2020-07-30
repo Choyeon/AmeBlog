c9e1ab1dbc776c7214d4461aa4d299408690c7ab
雨过初晴 <admin@choyeon.cn>
2020-07-30 16:35:32 +0800

    🐛修改模型类BUG

--- app/models.py
+++ app/models.py
@@ -29,16 +29,20 @@ class Post(db.Model):
     title = db.Column(db.String(60))
     body = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
-    category_id = db.Column(db.Integer, db.ForeignKey('category_id'))
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
     category = db.relationship('Category', back_populates='posts')
-    comments = db.relationship('Comment', backref='post', cascade='all,de;ete-orphan')
+    comments = db.relationship('Comment', back_populates='post', cascade='all, delete-orphan')
+
+    @classmethod
+    def count(cls):
+        return cls.query.count()
 
 
 # 分类模型类
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), unique=True)
-    post = db.relationship('Post', back_populates='category')
+    posts = db.relationship('Post', back_populates='category')
 
     @classmethod
     def count(cls):
@@ -55,8 +59,11 @@ class Comment(db.Model):
     from_admin = db.Column(db.Boolean, default=False)
     reviewed = db.Column(db.Boolean, default=False)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
-    post_id = db.Column(db.Integer, db.ForeignKey('Post'))
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
     post = db.relationship('Post', back_populates='comments')
+    replied_id = db.Column(db.Integer, db.ForeignKey('comment.id'))
+    replied = db.relationship('Comment', back_populates='replies', remote_side=[id])
+    replies = db.relationship('Comment', back_populates='replied', cascade='all')
 
     @classmethod
     def count(cls):
